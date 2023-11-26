31bfaaf843416f516fd392c4c9a299b103415262
Fohdil <fohdilulahiolayanju@gmail.com>
2023-11-26 19:34:12 +0000

    js

--- code_challenge/app.js
+++ code_challenge/app.js
@@ -1,8 +1,35 @@
 function notificationPopUp() {
-    let popup = document.getElementById("notification-m");
+    const popup = document.getElementById("notification-m");
     popup.style.display = (popup.style.display === "block") ? "none" : "block";
 }
 function openAdminPopUp() {
-    let popup = document.getElementById("adminpopup");
+    const popup = document.getElementById("adminpopup");
     popup.style.display = (popup.style.display === "block") ? "none" : "block";
 }
+function toggleArrow() {
+    const infodrop = document.getElementById("register-drop");
+    const arrowButton = document.querySelector('.arrow');
+    infodrop.classList.toggle('show')
+    arrowButton.style.transform = arrowButton.style.transform === 'rotate(180deg)' ? 'rotate(0deg)' : 'rotate(180deg)';
+}
+
+const setup = document.querySelectorAll('.setup')
+const itemshow = document.querySelectorAll(".info")
+
+setup.forEach((element, index) => {
+    element.addEventListener('click', () => {
+    itemshow.forEach(item => {
+        item.classList.remove('info-visible')
+    })
+    setup.forEach(item => {
+        item.classList.remove('active')
+    })
+    itemshow[index].classList.add('info-visible') 
+    setup[index].classList.add('active') 
+    })
+})
+// function showItem(){
+//     itemshow.forEach((element, index) => {
+//         itemshow[index].classList.add('info-visible')
+//     });
+// }
